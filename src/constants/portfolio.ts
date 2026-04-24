206eeec85c1efbb8e88a527f89d824cbbd4fbd06
AbishekVijay05 <abishekvijay05@gmail.com>
2026-04-24 21:33:08 +0530

    fix:add https to link

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -15,10 +15,10 @@ export const PERSONAL_INFO = {
   email: "abishekvijay05@gmail.com",
   phone: "+91 8015806129",
   github: "https://github.com/AbishekVijay05",
-  linkedin: "in/abisheka-priyan-v-6283ab311",
+  linkedin: "https://www.linkedin.com/in/abisheka-priyan-v-6283ab311",
   
 };
-  
+
 export const SKILLS = {
   languages: ["Java", "Python", "OOP", "PHP", "C", "C++", "JavaScript"],
   frameworks: ["React", "Flask", "HTML", "CSS"],
